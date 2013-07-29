8844725792a6e7c7185a29edebe888f24246d350
jmthompson2007@gmail.com <jmthompson2007@gmail.com@3b82c3e1-87b9-7bcc-34fc-2d08540c044f>
2013-07-29 19:57:57 +0000

    Initial version.

---
+++ core/doc/includes/AngloSaxonFontLetter.js
@@ -0,0 +1,83 @@
+
+// 3
+function THORN()
+{
+	document.write("&#5798;");
+}
+
+// 4
+function OS()
+{
+	document.write("&#5801;");
+}
+
+// 5
+function RAD()
+{
+	document.write("&#5809;");
+}
+
+// 6
+function CEN()
+{
+	document.write("&#5811;");
+}
+
+// 7
+function GYFU()
+{
+	document.write("&#5815;");
+}
+
+// 9
+function HAEGL()
+{
+	document.write("&#5819;");
+}
+
+// 10
+function NYD()
+{
+	document.write("&#5822;");
+}
+
+// 11
+function IS()
+{
+	document.write("&#5825;");
+}
+
+// 17
+function TIW()
+{
+	document.write("&#5839;");
+}
+
+// 18
+function BEORC()
+{
+	document.write("&#5842;");
+}
+
+// 19
+function EH()
+{
+	document.write("&#5846;");
+}
+
+// 24
+function DAEG()
+{
+	document.write("&#5854;");
+}
+
+// 26
+function AESC()
+{
+	document.write("&#5803;");
+}
+
+function SPACE()
+{
+	document.write("&#5867;");
+}
