d28c5081fa3aeb35e447fc888c8bdb617bd3ccbd
jmthompson2007@gmail.com <jmthompson2007@gmail.com@3b82c3e1-87b9-7bcc-34fc-2d08540c044f>
2013-07-31 19:44:34 +0000

    Initial version.

---
+++ trunk/core/src/main/java/org/runetranscriber/core/viking/AngloSaxonFontLetter.java
@@ -0,0 +1,107 @@
+package org.runetranscriber.core.viking;
+
+/**
+ * Provides Anglo-Saxon font letters.
+ */
+public final class AngloSaxonFontLetter
+{
+    /** Font letter. */
+    public static final String FEOH = Character.toString((char)5792);
+
+    /** Font letter. */
+    public static final String UR = Character.toString((char)5794);
+
+    /** Font letter. */
+    public static final String THORN = Character.toString((char)5798);
+
+    /** Font letter. */
+    public static final String OS = Character.toString((char)5801);
+
+    /** Font letter. */
+    public static final String RAD = Character.toString((char)5809);
+
+    /** Font letter. */
+    public static final String CEN = Character.toString((char)5811);
+
+    /** Font letter. */
+    public static final String GYFU = Character.toString((char)5815);
+
+    /** Font letter. */
+    public static final String WYNN = Character.toString((char)5817);
+
+    /** Font letter. */
+    public static final String HAEGL = Character.toString((char)5819);
+
+    /** Font letter. */
+    public static final String NYD = Character.toString((char)5822);
+
+    /** Font letter. */
+    public static final String IS = Character.toString((char)5825);
+
+    /** Font letter. */
+    public static final String GER = Character.toString((char)5828);
+
+    /** Font letter. */
+    public static final String EOH = Character.toString((char)5831);
+
+    /** Font letter. */
+    public static final String PEORD = Character.toString((char)5832);
+
+    /** Font letter. */
+    public static final String EOLH = Character.toString((char)5833);
+
+    /** Font letter. */
+    public static final String SIGEL = Character.toString((char)5835);
+
+    /** Font letter. */
+    public static final String TIW = Character.toString((char)5839);
+
+    /** Font letter. */
+    public static final String BEORC = Character.toString((char)5842);
+
+    /** Font letter. */
+    public static final String EH = Character.toString((char)5846);
+
+    /** Font letter. */
+    public static final String MANN = Character.toString((char)5847);
+
+    /** Font letter. */
+    public static final String LAGU = Character.toString((char)5850);
+
+    /** Font letter. */
+    public static final String ING = Character.toString((char)5853);
+
+    /** Font letter. */
+    public static final String EOEL = Character.toString((char)5855);
+
+    /** Font letter. */
+    public static final String DAEG = Character.toString((char)5854);
+
+    /** Font letter. */
+    public static final String AC = Character.toString((char)5802);
+
+    /** Font letter. */
+    public static final String AESC = Character.toString((char)5803);
+
+    /** Font letter. */
+    public static final String YR = Character.toString((char)5795);
+
+    /** Font letter. */
+    public static final String IOR = Character.toString((char)5820);
+
+    /** Font letter. */
+    public static final String EAR = Character.toString((char)5856);
+
+    /** Punctuation. */
+    /** Font letter. */
+    public static final String SPACE = Character.toString((char)5867);
+
+    /** Font letter. */
+    public static final String COMMA = Character.toString((char)5868);
+
+    /** Font letter. */
+    public static final String PERIOD = Character.toString((char)5010);
+
+    /** Font letter. */
+    public static final String NEWLINE = "\n";
+}
