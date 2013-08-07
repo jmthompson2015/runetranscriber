14e9f9d29c7500af28c31f9048a11ea729f4dba5
jmthompson2007@gmail.com <jmthompson2007@gmail.com@3b82c3e1-87b9-7bcc-34fc-2d08540c044f>
2013-08-07 19:33:53 +0000

    Added a string array constructor.

--- trunk/core/src/main/java/org/runetranscriber/core/FontLetterList.java
+++ trunk/core/src/main/java/org/runetranscriber/core/FontLetterList.java
@@ -28,6 +28,27 @@ public final class FontLetterList extends StringList
     /**
      * Construct this object.
      * 
+     * @param letters Letters.
+     */
+    public FontLetterList(final String... letters)
+    {
+        this();
+
+        if (letters != null)
+        {
+            for (int i = 0; i < letters.length; i++)
+            {
+                if (letters[i] != null)
+                {
+                    add(letters[i]);
+                }
+            }
+        }
+    }
+
+    /**
+     * Construct this object.
+     * 
      * @param fontLetters Initial font letters.
      */
     public FontLetterList(final String fontLetters)
