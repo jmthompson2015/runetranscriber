3e87ec0125464feb303c746431516a924b146980
jmthompson2007@gmail.com <jmthompson2007@gmail.com@3b82c3e1-87b9-7bcc-34fc-2d08540c044f>
2013-08-07 17:40:33 +0000

    Initial version.

---
+++ trunk/core/doc/includes/CirthFontLetter.js
@@ -0,0 +1,18 @@
+
+var CFL_E_DIAERESIS = "\u00EB";
+var CFL_SMALL_LETTER_ETH = "\u00F0";
+var CFL_U_MACRON = "\u016B";
+
+var CFL_ND = "D";
+var CFL_SOFT_VOWEL1 = "}";
+var CFL_SPACE = ".";
+
+function C43()
+{
+	document.write(CFL_U_MACRON);
+}
+
+function U_MACRON()
+{
+	document.write(CFL_U_MACRON);
+}
