b55b431184d5a35c4c5f80b66e01d2c1c729839e
jmthompson2007@gmail.com <jmthompson2007@gmail.com@3b82c3e1-87b9-7bcc-34fc-2d08540c044f>
2013-08-10 20:10:20 +0000

    Renamed Certh to CerthRune.

---
+++ trunk/core/src/main/java/org/runetranscriber/core/cirth/CerthRune.java
@@ -0,0 +1,328 @@
+package org.runetranscriber.core.cirth;
+
+import org.runetranscriber.core.DefaultRune;
+import org.runetranscriber.core.Rune;
+import org.runetranscriber.core.RuneList;
+
+/**
+ * Provides an enumeration of runes in the Cirth alphabet.
+ */
+public final class CerthRune implements Rune
+{
+    /** Rune. */
+    public static final CerthRune C1 = new CerthRune("C1");
+
+    /** Rune. */
+    public static final CerthRune C2 = new CerthRune("C2");
+
+    /** Rune. */
+    public static final CerthRune C3 = new CerthRune("C3");
+
+    /** Rune. */
+    public static final CerthRune C4 = new CerthRune("C4");
+
+    /** Rune. */
+    public static final CerthRune C5 = new CerthRune("C5");
+
+    /** Rune. */
+    public static final CerthRune C6 = new CerthRune("C6");
+
+    /** Rune. */
+    public static final CerthRune C7 = new CerthRune("C7");
+
+    /** Rune. */
+    public static final CerthRune C8 = new CerthRune("C8");
+
+    /** Rune. */
+    public static final CerthRune C9 = new CerthRune("C9");
+
+    /** Rune. */
+    public static final CerthRune C10 = new CerthRune("C10");
+
+    /** Rune. */
+    public static final CerthRune C11 = new CerthRune("C11");
+
+    /** Rune. */
+    public static final CerthRune C12 = new CerthRune("C12");
+
+    /** Rune. */
+    public static final CerthRune C13 = new CerthRune("C13");
+
+    /** Rune. */
+    public static final CerthRune C14 = new CerthRune("C14");
+
+    /** Rune. */
+    public static final CerthRune C15 = new CerthRune("C15");
+
+    /** Rune. */
+    public static final CerthRune C16 = new CerthRune("C16");
+
+    /** Rune. */
+    public static final CerthRune C17 = new CerthRune("C17");
+
+    /** Rune. */
+    public static final CerthRune C18 = new CerthRune("C18");
+
+    /** Rune. */
+    public static final CerthRune C19 = new CerthRune("C19");
+
+    /** Rune. */
+    public static final CerthRune C20 = new CerthRune("C20");
+
+    /** Rune. */
+    public static final CerthRune C21 = new CerthRune("C21");
+
+    /** Rune. */
+    public static final CerthRune C22 = new CerthRune("C22");
+
+    /** Rune. */
+    public static final CerthRune C23 = new CerthRune("C23");
+
+    /** Rune. */
+    public static final CerthRune C24 = new CerthRune("C24");
+
+    /** Rune. */
+    public static final CerthRune C25 = new CerthRune("C25");
+
+    /** Rune. */
+    public static final CerthRune C26 = new CerthRune("C26");
+
+    /** Rune. */
+    public static final CerthRune C27 = new CerthRune("C27");
+
+    /** Rune. */
+    public static final CerthRune C28 = new CerthRune("C28");
+
+    /** Rune. */
+    public static final CerthRune C29 = new CerthRune("C29");
+
+    /** Rune. */
+    public static final CerthRune C30 = new CerthRune("C30");
+
+    /** 31-45 */
+    public static final CerthRune C31 = new CerthRune("C31");
+
+    /** Rune. */
+    public static final CerthRune C32 = new CerthRune("C32");
+
+    /** Rune. */
+    public static final CerthRune C33 = new CerthRune("C33");
+
+    /** Rune. */
+    public static final CerthRune C34 = new CerthRune("C34");
+
+    /** Rune. */
+    public static final CerthRune C35 = new CerthRune("C35");
+
+    /** Rune. */
+    public static final CerthRune C36 = new CerthRune("C36");
+
+    /** Rune. */
+    public static final CerthRune C37 = new CerthRune("C37");
+
+    /** Rune. */
+    public static final CerthRune C38 = new CerthRune("C38");
+
+    /** Rune. */
+    public static final CerthRune C39 = new CerthRune("C39");
+
+    /** Rune. */
+    public static final CerthRune C40 = new CerthRune("C40");
+
+    /** Rune. */
+    public static final CerthRune C41 = new CerthRune("C41");
+
+    /** Rune. */
+    public static final CerthRune C42 = new CerthRune("C42");
+
+    /** Rune. */
+    public static final CerthRune C43 = new CerthRune("C43");
+
+    /** Rune. */
+    public static final CerthRune C44 = new CerthRune("C44");
+
+    /** Rune. */
+    public static final CerthRune C45 = new CerthRune("C45");
+
+    /** Rune. */
+    public static final CerthRune C46 = new CerthRune("C46");
+
+    /** Rune. */
+    public static final CerthRune C47 = new CerthRune("C47");
+
+    /** Rune. */
+    public static final CerthRune C48 = new CerthRune("C48");
+
+    /** Rune. */
+    public static final CerthRune C49 = new CerthRune("C49");
+
+    /** Rune. */
+    public static final CerthRune C50 = new CerthRune("C50");
+
+    /** Rune. */
+    public static final CerthRune C51 = new CerthRune("C51");
+
+    /** Rune. */
+    public static final CerthRune C52 = new CerthRune("C52");
+
+    /** Rune. */
+    public static final CerthRune C53 = new CerthRune("C53");
+
+    /** Rune. */
+    public static final CerthRune C54 = new CerthRune("C54");
+
+    /** Rune. */
+    public static final CerthRune C55 = new CerthRune("C55");
+
+    /** Rune. */
+    public static final CerthRune C56 = new CerthRune("C56");
+
+    /** Rune. */
+    public static final CerthRune C57 = new CerthRune("C57");
+
+    /** Rune. */
+    public static final CerthRune C58 = new CerthRune("C58");
+
+    /** Rune. */
+    public static final CerthRune C59 = new CerthRune("C59");
+
+    /** Rune. */
+    public static final CerthRune C60 = new CerthRune("C60");
+
+    /** Punctuation. */
+    /** Rune. */
+    public static final CerthRune SPACE = new CerthRune("SPACE");
+
+    /** Rune. */
+    public static final CerthRune COMMA = new CerthRune("COMMA");
+
+    /** Rune. */
+    public static final CerthRune PERIOD = new CerthRune("PERIOD");
+
+    /** Rune. */
+    public static final CerthRune NEWLINE = new CerthRune("NEWLINE");
+
+    /** Ordinal count. */
+    private static int ordinalCount = 0;
+
+    /** Values. */
+    private static RuneList<CerthRune> VALUES;
+
+    /**
+     * @param number Cirth number.
+     * 
+     * @return certh.
+     */
+    public final static CerthRune valueOfNumber(final int number)
+    {
+        CerthRune answer = null;
+
+        final RuneList<CerthRune> values = values();
+
+        if ((0 < number) && (number <= values.size()))
+        {
+            answer = values.get(number - 1);
+        }
+
+        return answer;
+    }
+
+    /**
+     * @return a copy of the list of rune values.
+     */
+    public static final RuneList<CerthRune> values()
+    {
+        return new RuneList<CerthRune>(VALUES);
+    }
+
+    /** Delegate. */
+    private final DefaultRune delegate;
+
+    /**
+     * Construct this object.
+     * 
+     * @param name Name.
+     */
+    private CerthRune(final String name)
+    {
+        final String displayName = name.toLowerCase();
+        delegate = new DefaultRune(ordinalCount, name, displayName, name);
+
+        if (VALUES == null)
+        {
+            VALUES = new RuneList<CerthRune>();
+        }
+
+        VALUES.add(this);
+        ordinalCount++;
+    }
+
+    @Override
+    public String displayName()
+    {
+        return delegate.displayName();
+    }
+
+    @Override
+    public boolean equals(final Object object)
+    {
+        boolean answer = false;
+
+        if (object == this)
+        {
+            answer = true;
+        }
+        else if (object == null)
+        {
+            answer = false;
+        }
+        else if (getClass() != object.getClass())
+        {
+            answer = false;
+        }
+        else
+        {
+            final CerthRune another = (CerthRune)object;
+
+            answer = delegate.equals(another.delegate);
+        }
+
+        return answer;
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return delegate.hashCode();
+    }
+
+    @Override
+    public String name()
+    {
+        return delegate.name();
+    }
+
+    @Override
+    public String nameMeaning()
+    {
+        return delegate.nameMeaning();
+    }
+
+    @Override
+    public int ordinal()
+    {
+        return delegate.ordinal();
+    }
+
+    @Override
+    public String toDebugString()
+    {
+        return delegate.toDebugString();
+    }
+
+    @Override
+    public String toString()
+    {
+        return delegate.toString();
+    }
+}
