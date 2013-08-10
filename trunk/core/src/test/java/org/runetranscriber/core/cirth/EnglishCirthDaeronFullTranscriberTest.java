290212fd176cd0da91cfcb074f3d39ce363d0d2c
jmthompson2007@gmail.com <jmthompson2007@gmail.com@3b82c3e1-87b9-7bcc-34fc-2d08540c044f>
2013-08-10 20:07:49 +0000

    Initial version.

---
+++ trunk/core/src/test/java/org/runetranscriber/core/cirth/EnglishCirthDaeronFullTranscriberTest.java
@@ -0,0 +1,106 @@
+package org.runetranscriber.core.cirth;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.runetranscriber.core.Example;
+import org.runetranscriber.core.FontLetterList;
+import org.runetranscriber.core.FullTranscriber;
+import org.runetranscriber.core.LanguageLetterList;
+import org.runetranscriber.core.cirth.example.Examples;
+
+/**
+ * Provides tests for the <code>EnglishCirthDaeronFullTranscriber</code> class.
+ */
+public final class EnglishCirthDaeronFullTranscriberTest
+{
+    /** Examples. */
+    private final Examples examples = new Examples();
+
+    /** Transcriber. */
+    private final FullTranscriber<Certh, CerthFontLetter> transcriber = new EnglishCirthDaeronFullTranscriber();
+
+    /**
+     * Test the <code>transcribeForward()</code> method.
+     */
+    @Test
+    public void transcribeForward1()
+    {
+        verifyTranscribeForward(examples.getCirthDaeron1());
+    }
+
+    /**
+     * Test the <code>transcribeForward()</code> method.
+     */
+    @Test
+    public void transcribeForward2()
+    {
+        verifyTranscribeForward(examples.getCirthDaeron2());
+    }
+
+    /**
+     * Test the <code>transcribeReverse()</code> method.
+     */
+    @Test
+    public void transcribeReverse1()
+    {
+        verifyTranscribeReverse(examples.getCirthDaeron1());
+    }
+
+    /**
+     * Test the <code>transcribeReverse()</code> method.
+     */
+    @Test
+    public void transcribeReverse2()
+    {
+        verifyTranscribeReverse(examples.getCirthDaeron2());
+    }
+
+    /**
+     * @param expected Expected language letters.
+     * 
+     * @return a string representation of the given parameter.
+     */
+    private String convert(final LanguageLetterList expected)
+    {
+        // Capital letters, apostrophes, and 'j' are lost in transcription.
+        String answer = expected.toString().toLowerCase();
+
+        answer = answer.replaceAll("[']", "");
+        answer = answer.replaceAll("c", "k");
+        answer = answer.replaceAll("j", "i");
+
+        return answer;
+    }
+
+    /**
+     * Test the <code>transcribeForward()</code> method.
+     * 
+     * @param example Example.
+     */
+    private void verifyTranscribeForward(final Example<Certh, CerthFontLetter> example)
+    {
+        final FontLetterList<CerthFontLetter> result = transcriber.transcribeForward(example.getLanguageLetters());
+
+        assertNotNull(result);
+        assertThat(result, is(example.getFontLetters()));
+    }
+
+    /**
+     * Test the <code>transcribeReverse()</code> method.
+     * 
+     * @param example Example.
+     */
+    private void verifyTranscribeReverse(final Example<Certh, CerthFontLetter> example)
+    {
+        final LanguageLetterList result = transcriber.transcribeReverse(example.getFontLetters());
+
+        assertNotNull(result);
+
+        final String expected = convert(example.getLanguageLetters());
+
+        assertThat(result.toString(), is(expected));
+    }
+}
---
+++ trunk/core/src/test/java/org/runetranscriber/core/cirth/KhuzdulCirthMoriaFullTranscriberTest.java
@@ -0,0 +1,88 @@
+package org.runetranscriber.core.cirth;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.runetranscriber.core.Example;
+import org.runetranscriber.core.FontLetterList;
+import org.runetranscriber.core.FullTranscriber;
+import org.runetranscriber.core.LanguageLetterList;
+import org.runetranscriber.core.cirth.example.Examples;
+
+/**
+ * Provides tests for the <code>KhuzdulCirthMoriaFullTranscriber</code> class.
+ */
+public final class KhuzdulCirthMoriaFullTranscriberTest
+{
+    /** Examples. */
+    private final Examples examples = new Examples();
+
+    /** Transcriber. */
+    private final FullTranscriber<Certh, CerthFontLetter> transcriber = new KhuzdulCirthMoriaFullTranscriber();
+
+    /**
+     * Test the <code>transcribeForward()</code> method.
+     */
+    @Test
+    public void transcribeForward1()
+    {
+        verifyTranscribeForward(examples.getCirthMoria1());
+    }
+
+    /**
+     * Test the <code>transcribeReverse()</code> method.
+     */
+    @Test
+    public void transcribeReverse1()
+    {
+        verifyTranscribeReverse(examples.getCirthMoria1());
+    }
+
+    /**
+     * @param expected Expected language letters.
+     * 
+     * @return a string representation of the given parameter.
+     */
+    private String convert(final LanguageLetterList expected)
+    {
+        // Capital letters, apostrophes, and 'j' are lost in transcription.
+        String answer = expected.toString().toLowerCase();
+
+        answer = answer.replaceAll("[']", "");
+        answer = answer.replaceAll("c", "k");
+        answer = answer.replaceAll("j", "i");
+
+        return answer;
+    }
+
+    /**
+     * Test the <code>transcribeForward()</code> method.
+     * 
+     * @param example Example.
+     */
+    private void verifyTranscribeForward(final Example<Certh, CerthFontLetter> example)
+    {
+        final FontLetterList<CerthFontLetter> result = transcriber.transcribeForward(example.getLanguageLetters());
+
+        assertNotNull(result);
+        assertThat(result, is(example.getFontLetters()));
+    }
+
+    /**
+     * Test the <code>transcribeReverse()</code> method.
+     * 
+     * @param example Example.
+     */
+    private void verifyTranscribeReverse(final Example<Certh, CerthFontLetter> example)
+    {
+        final LanguageLetterList result = transcriber.transcribeReverse(example.getFontLetters());
+
+        assertNotNull(result);
+
+        final String expected = convert(example.getLanguageLetters());
+
+        assertThat(result.toString(), is(expected));
+    }
+}
