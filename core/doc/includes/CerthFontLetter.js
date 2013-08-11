8cedb969c58a49b0ad6bc5db6f10ddb62f8a7c6c
jmthompson2007@gmail.com <jmthompson2007@gmail.com@3b82c3e1-87b9-7bcc-34fc-2d08540c044f>
2013-08-11 15:33:23 +0000

    Renamed file.

---
+++ core/doc/includes/CerthFontLetter.js
@@ -0,0 +1,170 @@
+
+var CFL_E_DIAERESIS = "\u00EB";
+var CFL_SMALL_LETTER_ETH = "\u00F0";
+var CFL_SMALL_LETTER_THORN = "\u00FE";
+var CFL_U_MACRON = "\u016B";
+
+function C1()
+{
+	document.write("p");
+}
+
+function C2()
+{
+	document.write("b");
+}
+
+function C3()
+{
+	document.write("f");
+}
+
+function C4()
+{
+	document.write("v");
+}
+
+function C5()
+{
+	document.write("W");
+}
+
+function C6()
+{
+	document.write("m");
+}
+
+function C7()
+{
+	document.write("M");
+}
+
+function C8()
+{
+	document.write("t");
+}
+
+function C9()
+{
+	document.write("d");
+}
+
+function C10()
+{
+	document.write(CFL_SMALL_LETTER_THORN);
+}
+
+function C11()
+{
+	document.write(CFL_SMALL_LETTER_ETH);
+}
+
+function C12()
+{
+	document.write("r");
+}
+
+function C17()
+{
+	document.write("z");
+}
+
+function C18()
+{
+	document.write("k");
+}
+
+function C19()
+{
+	document.write("g");
+}
+
+function C22()
+{
+	document.write("n");
+}
+
+function C29()
+{
+	document.write("J");
+}
+
+function C31()
+{
+	document.write("l");
+}
+
+function C33()
+{
+	document.write("D");
+}
+
+function C35()
+{
+	document.write("s");
+}
+
+function C36()
+{
+	document.write("N");
+}
+
+function C39()
+{
+	document.write("i");
+}
+
+function C42()
+{
+	document.write("u");
+}
+
+function C43()
+{
+	document.write("U");
+}
+
+function C46()
+{
+	document.write("e");
+}
+
+function C48()
+{
+	document.write("a");
+}
+
+function C50()
+{
+	document.write("o");
+}
+
+function C51()
+{
+	document.write("O");
+}
+
+function C55()
+{
+	document.write(CFL_E_DIAERESIS);
+}
+
+function C56()
+{
+	document.write("}");
+}
+
+function C59()
+{
+	document.write("|");
+}
+
+function C_SPACE()
+{
+	document.write(".");
+}
+
+function U_MACRON()
+{
+	document.write(CFL_U_MACRON);
+}
