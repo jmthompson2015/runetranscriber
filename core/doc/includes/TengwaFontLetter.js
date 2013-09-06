2c037aef7d83bd82cb19557f4be86ff0742a3cbc
jmthompson2007@gmail.com <jmthompson2007@gmail.com@3b82c3e1-87b9-7bcc-34fc-2d08540c044f>
2013-09-06 16:39:42 +0000

    Initial version.

---
+++ core/doc/includes/TengwaFontLetter.js
@@ -0,0 +1,522 @@
+
+// 1
+function TINCO()
+{
+	document.write("1");
+}
+
+// 2
+function PARMA()
+{
+	document.write("q");
+}
+
+// 3
+function CALMA()
+{
+	document.write("a");
+}
+
+// 4
+function QUESSE()
+{
+	document.write("z");
+}
+
+// 5
+function ANDO()
+{
+	document.write("2");
+}
+
+// 6
+function UMBAR()
+{
+	document.write("w");
+}
+
+// 7
+function ANGA()
+{
+	document.write("s");
+}
+
+// 8
+function UNGWE()
+{
+	document.write("x");
+}
+
+// 9
+function THULE()
+{
+	document.write("3");
+}
+
+// 10
+function FORMEN()
+{
+	document.write("e");
+}
+
+// 11
+function HARMA()
+{
+	document.write("d");
+}
+
+// 12
+function HWESTA()
+{
+	document.write("c");
+}
+
+// 13
+function ANTO()
+{
+	document.write("4");
+}
+
+// 14
+function AMPA()
+{
+	document.write("r");
+}
+
+// 15
+function ANCA()
+{
+	document.write("f");
+}
+
+// 16
+function UNQUE()
+{
+	document.write("v");
+}
+
+// 17
+function NUMEN()
+{
+	document.write("5");
+}
+
+// 18
+function MALTA()
+{
+	document.write("t");
+}
+
+// 19
+function NGOLDO()
+{
+	document.write("g");
+}
+
+// 20
+function NGWALME()
+{
+	document.write("b");
+}
+
+// 21
+function ORE()
+{
+	document.write("6");
+}
+
+// 22
+function VALA()
+{
+	document.write("y");
+}
+
+// 23
+function ANNA()
+{
+	document.write("h");
+}
+
+// 24
+function VILYA()
+{
+	document.write("n");
+}
+
+// 25
+function ROMEN()
+{
+	document.write("7");
+}
+
+// 26
+function ARDA()
+{
+	document.write("u");
+}
+
+// 27
+function LAMBE()
+{
+	document.write("j");
+}
+
+// 28
+function ALDA()
+{
+	document.write("m");
+}
+
+// 29
+function SILME()
+{
+	document.write("8");
+}
+
+// 30
+function SILME_NUQUERNA()
+{
+	document.write("i");
+}
+
+// 31
+function ESSE()
+{
+	document.write("k");
+}
+
+// 32
+function ESSE_NUQUERNA()
+{
+	document.write(",");
+}
+
+// 33
+function HYARMEN()
+{
+	document.write("9");
+}
+
+// 34
+function HWESTA_SINDARINWA()
+{
+	document.write("o");
+}
+
+// 35
+function YANTA()
+{
+	document.write("l");
+}
+
+// 36
+function URE()
+{
+	document.write(".");
+}
+
+// 37
+function THE()
+{
+	document.write("@");
+}
+
+// 38
+function OF()
+{
+	document.write("W");
+}
+
+// 39
+function OF_THE()
+{
+	document.write("W?");
+}
+
+// 40
+function SHORT_CARRIER()
+{
+	document.write("`");
+}
+
+// 41
+function LONG_CARRIER()
+{
+	document.write("~");
+}
+
+// 42
+function THREE_DOTS1()
+{
+	document.write("E");
+}
+
+// 43
+function THREE_DOTS2()
+{
+	document.write("D");
+}
+
+// 44
+function THREE_DOTS3()
+{
+	document.write("C");
+}
+
+// 45
+function THREE_UNDER_DOTS1()
+{
+	document.write("\u00d0");
+}
+
+// 46
+function THREE_UNDER_DOTS2()
+{
+	document.write("\u00d1");
+}
+
+// 47
+function THREE_UNDER_DOTS3()
+{
+	document.write("\u00d2");
+}
+
+// 48
+function THREE_UNDER_DOTS4()
+{
+	document.write("\u00d3");
+}
+
+// 49
+function THREE_UNDER_DOTS5()
+{
+	document.write(">");
+}
+
+// 50
+function CIRCUMFLEX1()
+{
+	document.write("\u00dc");
+}
+
+// 51
+function CIRCUMFLEX2()
+{
+	document.write("\u00dd");
+}
+
+// 52
+function CIRCUMFLEX3()
+{
+	document.write("\u00de");
+}
+
+// 53
+function CIRCUMFLEX4()
+{
+	document.write("\u00df");
+}
+
+// 54
+function ACUTE1()
+{
+	document.write("R");
+}
+
+// 55
+function ACUTE2()
+{
+	document.write("F");
+}
+
+// 56
+function ACUTE3()
+{
+	document.write("V");
+}
+
+// 57
+function DOT1()
+{
+	document.write("T");
+}
+
+// 58
+function DOT2()
+{
+	document.write("G");
+}
+
+// 59
+function DOT3()
+{
+	document.write("B");
+}
+
+// 60
+function UNDER_DOT1()
+{
+	document.write("\u00c8");
+}
+
+// 61
+function UNDER_DOT2()
+{
+	document.write("\u00c9");
+}
+
+// 62
+function UNDER_DOT3()
+{
+	document.write("\u00ca");
+}
+
+// 63
+function UNDER_DOT4()
+{
+	document.write("\u00cb");
+}
+
+// 64
+function UNDER_DOT5()
+{
+	document.write("O");
+}
+
+// 65
+function LEFT_CURL1()
+{
+	document.write("^");
+}
+
+// 66
+function LEFT_CURL2()
+{
+	document.write("Y");
+}
+
+// 67
+function LEFT_CURL3()
+{
+	document.write("N");
+}
+
+// 68
+function LEFT_CURL4()
+{
+	document.write("H");
+}
+
+// 69
+function LEFT_UNDER_CURL1()
+{
+	document.write("\u00e4");
+}
+
+// 70
+function LEFT_UNDER_CURL2()
+{
+	document.write("\u00e5");
+}
+
+// 71
+function LEFT_UNDER_CURL3()
+{
+	document.write("\u00e6");
+}
+
+// 72
+function LEFT_UNDER_CURL4()
+{
+	document.write("\u00e7");
+}
+
+// 73
+function TOP_CURL1()
+{
+	document.write("U");
+}
+
+// 74
+function TOP_CURL2()
+{
+	document.write("J");
+}
+
+// 75
+function TOP_CURL3()
+{
+	document.write("M");
+}
+
+// 76
+function RIGHT_CURL1()
+{
+	document.write("\u00e0");
+}
+
+// 77
+function RIGHT_CURL2()
+{
+	document.write("\u00e1");
+}
+
+// 78
+function RIGHT_CURL3()
+{
+	document.write("\u00e2");
+}
+
+// 79
+function RIGHT_CURL4()
+{
+	document.write("\u00e3");
+}
+
+// 80
+function CHEVRON1()
+{
+	document.write("\u00d8");
+}
+
+// 81
+function CHEVRON2()
+{
+	document.write("\u00d9");
+}
+
+// 82
+function CHEVRON3()
+{
+	document.write("\u00da");
+}
+
+// 83
+function CHEVRON4()
+{
+	document.write("\u00db");
+}
+
+// 84
+function SPACE()
+{
+	document.write("\u0020");
+}
+
+// 85
+function COMMA()
+{
+	document.write("\u00b7");
+}
+
+// 86
+function PERIOD()
+{
+	document.write("-");
+}
+
+// 87
+function NEWLINE()
+{
+	document.write("<br/>");
+}
