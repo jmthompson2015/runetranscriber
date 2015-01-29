54c7d34c84fbd4f503bd86f41c132d8f3d4caa1f
jmthompson2007@gmail.com <jmthompson2007@gmail.com@3b82c3e1-87b9-7bcc-34fc-2d08540c044f>
2015-01-29 17:28:50 +0000

    Initial version.

---
+++ trunk/webapp/src/main/js/example/TengwarExampleToHtml.js
@@ -0,0 +1,140 @@
+var TengwarExampleToHtml = function(runesIn)
+{
+    var runes = runesIn;
+    var transcriber = new EnglishTengwarTranscriber2();
+
+    this.getRunes = function()
+    {
+        return runes;
+    }
+
+    this.getTranscriber = function()
+    {
+        return transcriber;
+    }
+}
+
+TengwarExampleToHtml.prototype.generateFontRows = function()
+{
+    var letters = this.getTranscriber().runesToFontLetters(this.getRunes());
+
+    var answer = "<tr>\n";
+    answer += "<td class=\"table-header-cell-details\">Font</td>\n";
+
+    for (var i = 0; i < letters.length; i++)
+    {
+        var letter = letters[i];
+
+        if (letter !== "newline")
+        {
+            answer += "<td class=\"table-cell-tengwar\">"
+
+            if (Array.isArray(letter))
+            {
+                for (var j = 0; j < letter.length; j++)
+                {
+                    var letter2 = letter[j];
+                    answer += letter2;
+                }
+            }
+            else
+            {
+                answer += letter;
+            }
+
+            answer += "</td>\n"
+        }
+    }
+
+    answer += "</tr>\n";
+
+    return answer;
+}
+
+TengwarExampleToHtml.prototype.generateRuneRows = function()
+{
+    var runes = this.getRunes();
+    var properties = TengwaRune.properties;
+
+    var answer = "<tr>\n";
+    answer += "<td class=\"table-header-cell-details\">Rune</td>\n";
+
+    for (var i = 0; i < runes.length; i++)
+    {
+        var rune = runes[i];
+
+        if (rune !== "newline")
+        {
+            answer += "<td class=\"table-cell-rune-name\">";
+
+            if (rune === "space")
+            {
+                answer += " ";
+            }
+            else if (rune === "period")
+            {
+                answer += ".";
+            }
+            else if (Array.isArray(rune))
+            {
+                for (var j = 0; j < rune.length; j++)
+                {
+                    var rune2 = rune[j];
+                    answer += properties[rune2].name;
+
+                    if (j < rune.length - 1)
+                    {
+                        answer += "-";
+                    }
+                }
+            }
+            else
+            {
+                answer += properties[rune].name;
+            }
+
+            answer += "</td>\n";
+        }
+    }
+
+    answer += "</tr>\n";
+
+    return answer;
+}
+
+TengwarExampleToHtml.prototype.generateLanguageRows = function()
+{
+    var letters = this.getTranscriber().runesToLanguageLetters(this.getRunes());
+
+    var answer = "<tr>\n";
+    answer += "<td class=\"table-header-cell-details\">Language</td>\n";
+
+    for (var i = 0; i < letters.length; i++)
+    {
+        var letter = letters[i];
+
+        if (letter !== "newline")
+        {
+            answer += "<td class=\"table-cell-details\">";
+
+            if (Array.isArray(letter))
+            {
+                for (var j = 0; j < letter.length; j++)
+                {
+                    var letter2 = letter[j];
+                    answer += letter2;
+                }
+            }
+            else
+            {
+                answer += letter;
+            }
+
+            answer += "</td>\n";
+        }
+    }
+
+    answer += "</tr>\n";
+
+    return answer;
+}
