a5ec0685b49ae59b321af74749b92fff0995b9d7
jmthompson2007@gmail.com <jmthompson2007@gmail.com@3b82c3e1-87b9-7bcc-34fc-2d08540c044f>
2015-02-19 17:03:33 +0000

    Initial version.

---
+++ webapp/src/main/js/AurebeshRune.js
@@ -0,0 +1,468 @@
+/*
+ * Provides an enumeration of letters in the Aurebesh alphabet.
+ */
+var AurebeshRune =
+{
+    AUREK: "aurek",
+    BESH: "besh",
+    CRESH: "cresh",
+    DORN: "dorn",
+    ESK: "esk",
+    FORN: "forn",
+    GREK: "grek",
+    HERF: "herf",
+    ISK: "isk",
+    JENTH: "jenth",
+    KRILL: "krill",
+    LETH: "leth",
+    MERN: "mern",
+    NERN: "nern",
+    OSK: "osk",
+    PETH: "peth",
+    QEK: "qek",
+    RESH: "resh",
+    SENTH: "senth",
+    TRILL: "trill",
+    USK: "usk",
+    VEV: "vev",
+    WESK: "wesk",
+    XESH: "xesh",
+    YIRT: "yirt",
+    ZEREK: "zerek",
+    CHEREK: "cherek",
+    ENTH: "enth",
+    ONITH: "onith",
+    KRENTH: "krenth",
+    NEN: "nen",
+    ORENTH: "orenth",
+    SHEN: "shen",
+    THESH: "thesh",
+    ZERO: "zero",
+    ONE: "one",
+    TWO: "two",
+    THREE: "three",
+    FOUR: "four",
+    FIVE: "five",
+    SIX: "six",
+    SEVEN: "seven",
+    EIGHT: "eight",
+    NINE: "nine",
+    PLUS: "plus",
+    MINUS: "minus",
+    HYPHEN: "hyphen",
+    LEFT_PAREN: "leftParen",
+    RIGHT_PAREN: "rightParen",
+    SPACE: "space",
+    COMMA: "comma",
+    PERIOD: "period",
+    NEWLINE: "newline",
+    properties:
+    {
+        "aurek":
+        {
+            name: "AUREK",
+            displayName: "aurek",
+            fontLetter: "a",
+            phoneme: "a",
+        },
+        "besh":
+        {
+            name: "BESH",
+            displayName: "besh",
+            fontLetter: "b",
+            phoneme: "b",
+        },
+        "cresh":
+        {
+            name: "CRESH",
+            displayName: "cresh",
+            fontLetter: "c",
+            phoneme: "c",
+        },
+        "dorn":
+        {
+            name: "DORN",
+            displayName: "dorn",
+            fontLetter: "d",
+            phoneme: "d",
+        },
+        "esk":
+        {
+            name: "ESK",
+            displayName: "esk",
+            fontLetter: "e",
+            phoneme: "e",
+        },
+        "forn":
+        {
+            name: "FORN",
+            displayName: "forn",
+            fontLetter: "f",
+            phoneme: "f",
+        },
+        "grek":
+        {
+            name: "GREK",
+            displayName: "grek",
+            fontLetter: "g",
+            phoneme: "g",
+        },
+        "herf":
+        {
+            name: "HERF",
+            displayName: "herf",
+            fontLetter: "h",
+            phoneme: "h",
+        },
+        "isk":
+        {
+            name: "ISK",
+            displayName: "isk",
+            fontLetter: "i",
+            phoneme: "i",
+        },
+        "jenth":
+        {
+            name: "JENTH",
+            displayName: "jenth",
+            fontLetter: "j",
+            phoneme: "j",
+        },
+        "krill":
+        {
+            name: "KRILL",
+            displayName: "krill",
+            fontLetter: "k",
+            phoneme: "k",
+        },
+        "leth":
+        {
+            name: "LETH",
+            displayName: "leth",
+            fontLetter: "l",
+            phoneme: "l",
+        },
+        "mern":
+        {
+            name: "MERN",
+            displayName: "mern",
+            fontLetter: "m",
+            phoneme: "m",
+        },
+        "nern":
+        {
+            name: "NERN",
+            displayName: "nern",
+            fontLetter: "n",
+            phoneme: "n",
+        },
+        "osk":
+        {
+            name: "OSK",
+            displayName: "osk",
+            fontLetter: "o",
+            phoneme: "o",
+        },
+        "peth":
+        {
+            name: "PETH",
+            displayName: "peth",
+            fontLetter: "p",
+            phoneme: "p",
+        },
+        "qek":
+        {
+            name: "QEK",
+            displayName: "qek",
+            fontLetter: "q",
+            phoneme: "q",
+        },
+        "resh":
+        {
+            name: "RESH",
+            displayName: "resh",
+            fontLetter: "r",
+            phoneme: "r",
+        },
+        "senth":
+        {
+            name: "SENTH",
+            displayName: "senth",
+            fontLetter: "s",
+            phoneme: "s",
+        },
+        "trill":
+        {
+            name: "TRILL",
+            displayName: "trill",
+            fontLetter: "t",
+            phoneme: "t",
+        },
+        "usk":
+        {
+            name: "USK",
+            displayName: "usk",
+            fontLetter: "u",
+            phoneme: "u",
+        },
+        "vev":
+        {
+            name: "VEV",
+            displayName: "vev",
+            fontLetter: "v",
+            phoneme: "v",
+        },
+        "wesk":
+        {
+            name: "WESK",
+            displayName: "wesk",
+            fontLetter: "w",
+            phoneme: "w",
+        },
+        "xesh":
+        {
+            name: "XESH",
+            displayName: "xesh",
+            fontLetter: "x",
+            phoneme: "x",
+        },
+        "yirt":
+        {
+            name: "YIRT",
+            displayName: "yirt",
+            fontLetter: "y",
+            phoneme: "y",
+        },
+        "zerek":
+        {
+            name: "ZEREK",
+            displayName: "zerek",
+            fontLetter: "z",
+            phoneme: "z",
+        },
+        "cherek":
+        {
+            name: "CHEREK",
+            displayName: "cherek",
+            fontLetter: "&#231;",
+            phoneme: "ch",
+        },
+        "enth":
+        {
+            name: "ENTH",
+            displayName: "enth",
+            fontLetter: "&#230;",
+            phoneme: "ae",
+        },
+        "onith":
+        {
+            name: "ONITH",
+            displayName: "onith",
+            fontLetter: "&#235;",
+            phoneme: "eo",
+        },
+        "krenth":
+        {
+            name: "KRENTH",
+            displayName: "krenth",
+            fontLetter: "&#254;",
+            phoneme: "kh",
+        },
+        "nen":
+        {
+            name: "NEN",
+            displayName: "nen",
+            fontLetter: "&#241;",
+            phoneme: "ng",
+        },
+        "orenth":
+        {
+            name: "ORENTH",
+            displayName: "orenth",
+            fontLetter: "&#248;",
+            phoneme: "oo",
+        },
+        "shen":
+        {
+            name: "SHEN",
+            displayName: "shen",
+            fontLetter: "&#223;",
+            phoneme: "sh",
+        },
+        "thesh":
+        {
+            name: "THESH",
+            displayName: "thesh",
+            fontLetter: "&#240;",
+            phoneme: "th",
+        },
+        "zero":
+        {
+            name: "ZERO",
+            displayName: "zero",
+            fontLetter: "0",
+            phoneme: "0",
+        },
+        "one":
+        {
+            name: "ONE",
+            displayName: "one",
+            fontLetter: "1",
+            phoneme: "1",
+        },
+        "two":
+        {
+            name: "TWO",
+            displayName: "two",
+            fontLetter: "2",
+            phoneme: "2",
+        },
+        "three":
+        {
+            name: "THREE",
+            displayName: "three",
+            fontLetter: "3",
+            phoneme: "3",
+        },
+        "four":
+        {
+            name: "FOUR",
+            displayName: "four",
+            fontLetter: "4",
+            phoneme: "4",
+        },
+        "five":
+        {
+            name: "FIVE",
+            displayName: "five",
+            fontLetter: "5",
+            phoneme: "5",
+        },
+        "six":
+        {
+            name: "SIX",
+            displayName: "six",
+            fontLetter: "6",
+            phoneme: "6",
+        },
+        "seven":
+        {
+            name: "SEVEN",
+            displayName: "seven",
+            fontLetter: "7",
+            phoneme: "7",
+        },
+        "eight":
+        {
+            name: "EIGHT",
+            displayName: "eight",
+            fontLetter: "8",
+            phoneme: "8",
+        },
+        "nine":
+        {
+            name: "NINE",
+            displayName: "nine",
+            fontLetter: "9",
+            phoneme: "9",
+        },
+        "plus":
+        {
+            name: "PLUS",
+            displayName: "plus",
+            fontLetter: "+",
+            phoneme: "+",
+        },
+        "minus":
+        {
+            name: "MINUS",
+            displayName: "minus",
+            fontLetter: "-",
+            phoneme: "-",
+        },
+        "hyphen":
+        {
+            name: "HYPHEN",
+            displayName: "hyphen",
+            fontLetter: "-",
+            phoneme: "-",
+        },
+        "leftParen":
+        {
+            name: "LEFT_PAREN",
+            displayName: "leftParen",
+            fontLetter: "(",
+            phoneme: "(",
+        },
+        "rightParen":
+        {
+            name: "RIGHT_PAREN",
+            displayName: "rightParen",
+            fontLetter: ")",
+            phoneme: ")",
+        },
+        "space":
+        {
+            name: "SPACE",
+            displayName: "space",
+            fontLetter: " ",
+            phoneme: " ",
+        },
+        "comma":
+        {
+            name: "COMMA",
+            displayName: "comma",
+            fontLetter: ",",
+            phoneme: ",",
+        },
+        "period":
+        {
+            name: "PERIOD",
+            displayName: "period",
+            fontLetter: ".",
+            phoneme: ".",
+        },
+        "newline":
+        {
+            name: "NEWLINE",
+            displayName: "newline",
+            fontLetter: "\n",
+            phoneme: "\n",
+        },
+    },
+    values: [ // values
+    "aurek", "besh", "cresh", "dorn", "esk", "forn", "grek", // 1
+    "herf", "isk", "jenth", "krill", "leth", "mern", "nern", // 2
+    "osk", "peth", "qek", "resh", "senth", "trill", "usk", // 3
+    "vev", "wesk", "xesh", "yirt", "zerek", "cherek", "enth", // 4
+    "onith", "krenth", "nen", "orenth", "shen", "thesh", // 5
+    "one", "two", "three", "four", "five", "six", // numbers 1
+    "seven", "eight", "nine", "zero", "plus", "minus", // numbers 2
+    "hyphen", "leftParen", "rightParen", "space", "comma", "period", "newline", // punctuation
+    ],
+
+    findRuneByPhoneme: function(phoneme)
+    {
+        var answer;
+        var values = AurebeshRune.values;
+        var properties = AurebeshRune.properties;
+
+        for (var j = 0; j < values.length; j++)
+        {
+            rune = values[j];
+
+            if (properties[rune].phoneme === phoneme)
+            {
+                answer = rune;
+                break;
+            }
+        }
+
+        return answer;
+    },
+}
+
+if (Object.freeze)
+{
+    Object.freeze(AurebeshRune)
+};
