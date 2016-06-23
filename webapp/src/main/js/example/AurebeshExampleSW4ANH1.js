4c79f4489595ece4512f162ea8177e4ab3f9727f
Jeffrey M. Thompson <jmthompson@192.168.1.9>
2016-06-23 16:29:45 -0600

    Changed URLs to point to github.

--- webapp/src/main/js/example/AurebeshExampleSW4ANH1.js
+++ webapp/src/main/js/example/AurebeshExampleSW4ANH1.js
@@ -3,27 +3,23 @@
  */
 var AurebeshExampleSW4ANH1 =
 {
-    IMAGE: "https://runetranscriber.googlecode.com/svn/trunk/core/doc/images/starwars/SW4ANH_tractorBeam.jpg",
+    IMAGE: "https://rawgit.com/jmthompson2015/runetranscriber/master/core/doc/images/starwars/SW4ANH_tractorBeam.jpg",
 
     LANGUAGE_STRING: "power\n" + "tractor beam 12 sec n6",
 
     RUNES: [
     // Line 1
     // power
-    AurebeshRune.PETH, AurebeshRune.OSK, AurebeshRune.WESK, AurebeshRune.ESK,
-            AurebeshRune.RESH, AurebeshRune.NEWLINE,
-            // tractor
-            AurebeshRune.TRILL, AurebeshRune.RESH, AurebeshRune.AUREK,
-            AurebeshRune.CRESH, AurebeshRune.TRILL, AurebeshRune.OSK,
-            AurebeshRune.RESH, AurebeshRune.SPACE,
+    AurebeshRune.PETH, AurebeshRune.OSK, AurebeshRune.WESK, AurebeshRune.ESK, AurebeshRune.RESH, AurebeshRune.NEWLINE,
+    // tractor
+    AurebeshRune.TRILL, AurebeshRune.RESH, AurebeshRune.AUREK, AurebeshRune.CRESH, AurebeshRune.TRILL,
+            AurebeshRune.OSK, AurebeshRune.RESH, AurebeshRune.SPACE,
             // beam
-            AurebeshRune.BESH, AurebeshRune.ESK, AurebeshRune.AUREK,
-            AurebeshRune.MERN, AurebeshRune.SPACE,
+            AurebeshRune.BESH, AurebeshRune.ESK, AurebeshRune.AUREK, AurebeshRune.MERN, AurebeshRune.SPACE,
             // 12
             AurebeshRune.ONE, AurebeshRune.TWO, AurebeshRune.SPACE,
             // sec
-            AurebeshRune.SENTH, AurebeshRune.ESK, AurebeshRune.CRESH,
-            AurebeshRune.SPACE,
+            AurebeshRune.SENTH, AurebeshRune.ESK, AurebeshRune.CRESH, AurebeshRune.SPACE,
             // n6)
             AurebeshRune.NERN, AurebeshRune.SIX, ],
 }
--- webapp/src/main/js/example/TengwarExampleSD1.js
+++ webapp/src/main/js/example/TengwarExampleSD1.js
@@ -3,120 +3,90 @@
  */
 var TengwarExampleSD1 =
 {
-    IMAGE: "https://runetranscriber.googlecode.com/svn/trunk/core/doc/images/saurondefeated/SD_TitlePage1.jpg",
+    IMAGE: "https://rawgit.com/jmthompson2015/runetranscriber/master/core/doc/images/saurondefeated/SD_TitlePage1.jpg",
 
     LANGUAGE_STRING: "In this book is traced first the story of the destruction of the\n"
             + "One Ring and the Downfall of Sauron at the End of the Third Age.",
 
-    LANGUAGE_WORDS: [ "in", " ", "this", " ", "book", " ", "is", " ", "traced",
-            " ", "first", " ", "the", " ", "story", " ", "of the", " ",
-            "destruction", " ", "of the", "\n", "one", " ", "ring", " ", "and",
-            " ", "the", " ", "downfall", " ", "of", " ", "sauron", " ", "at",
-            " ", "the", " ", "end", " ", "of the", " ", "third", " ", "age",
-            "." ],
+    LANGUAGE_WORDS: [ "in", " ", "this", " ", "book", " ", "is", " ", "traced", " ", "first", " ", "the", " ", "story",
+            " ", "of the", " ", "destruction", " ", "of the", "\n", "one", " ", "ring", " ", "and", " ", "the", " ",
+            "downfall", " ", "of", " ", "sauron", " ", "at", " ", "the", " ", "end", " ", "of the", " ", "third", " ",
+            "age", "." ],
 
-    PHONEMES: [ [ "i", "n" ], " ", "th", [ "i", "s" ], " ", "b", "oo", "k",
-            " ", [ "i", "s" ], " ", "t", "r", [ "a", "c", "e" ], "d", " ", "f",
-            [ "i", "r" ], "s", "t", " ", "the", " ", "s", "t", [ "o", "r" ],
-            "y", " ", "of the", " ", "d", [ "e", "s" ], "t", "r", [ "u", "k" ],
-            "t", "i", [ "o", "n" ], " ", "of the", "\n", [ "o", "n", "e" ],
-            " ", "r", [ "i", "ng" ], " ", "and", " ", "the", " ", "d",
-            [ "o", "w" ], "n", "f", [ "a", "l", "l" ], " ", "of", " ", "s",
-            "a", [ "u", "r" ], [ "o", "n" ], " ", [ "a", "t" ], " ", "the",
-            " ", [ "e", "n" ], "d", " ", "of the", " ", "th", [ "i", "r" ],
-            "d", " ", [ "a", "j", "e" ], "." ],
+    PHONEMES: [ [ "i", "n" ], " ", "th", [ "i", "s" ], " ", "b", "oo", "k", " ", [ "i", "s" ], " ", "t", "r",
+            [ "a", "c", "e" ], "d", " ", "f", [ "i", "r" ], "s", "t", " ", "the", " ", "s", "t", [ "o", "r" ], "y",
+            " ", "of the", " ", "d", [ "e", "s" ], "t", "r", [ "u", "k" ], "t", "i", [ "o", "n" ], " ", "of the", "\n",
+            [ "o", "n", "e" ], " ", "r", [ "i", "ng" ], " ", "and", " ", "the", " ", "d", [ "o", "w" ], "n", "f",
+            [ "a", "l", "l" ], " ", "of", " ", "s", "a", [ "u", "r" ], [ "o", "n" ], " ", [ "a", "t" ], " ", "the",
+            " ", [ "e", "n" ], "d", " ", "of the", " ", "th", [ "i", "r" ], "d", " ", [ "a", "j", "e" ], "." ],
 
     RUNES: [
-            // Line 1
-            // in
-            [ TengwaRune.ACUTE, TengwaRune.NUMEN ],
-            TengwaRune.SPACE,
-            // this
-            TengwaRune.ANTO,
-            [ TengwaRune.ACUTE, TengwaRune.SILME ],
-            TengwaRune.SPACE,
-            // book
-            TengwaRune.UMBAR,
-            [ TengwaRune.LEFT_CURL, TengwaRune.LONG_CARRIER ],
-            TengwaRune.QUESSE,
-            TengwaRune.SPACE,
-            // is
-            [ TengwaRune.ACUTE, TengwaRune.SILME ],
-            TengwaRune.SPACE,
-            // traced
-            TengwaRune.TINCO,
-            TengwaRune.ROMEN,
-            [ TengwaRune.THREE_DOTS, TengwaRune.SILME_NUQUERNA,
-                    TengwaRune.UNDER_DOT ], TengwaRune.ANDO, TengwaRune.SPACE,
+    // Line 1
+    // in
+    [ TengwaRune.ACUTE, TengwaRune.NUMEN ], TengwaRune.SPACE,
+    // this
+    TengwaRune.ANTO, [ TengwaRune.ACUTE, TengwaRune.SILME ], TengwaRune.SPACE,
+    // book
+    TengwaRune.UMBAR, [ TengwaRune.LEFT_CURL, TengwaRune.LONG_CARRIER ], TengwaRune.QUESSE, TengwaRune.SPACE,
+    // is
+    [ TengwaRune.ACUTE, TengwaRune.SILME ], TengwaRune.SPACE,
+    // traced
+    TengwaRune.TINCO, TengwaRune.ROMEN, [ TengwaRune.THREE_DOTS, TengwaRune.SILME_NUQUERNA, TengwaRune.UNDER_DOT ],
+            TengwaRune.ANDO, TengwaRune.SPACE,
             // first
-            TengwaRune.FORMEN, [ TengwaRune.ACUTE, TengwaRune.ORE ],
-            TengwaRune.SILME, TengwaRune.TINCO, TengwaRune.SPACE,
+            TengwaRune.FORMEN, [ TengwaRune.ACUTE, TengwaRune.ORE ], TengwaRune.SILME, TengwaRune.TINCO,
+            TengwaRune.SPACE,
             // the
             TengwaRune.THE, TengwaRune.SPACE,
             // story
-            TengwaRune.SILME, TengwaRune.TINCO,
-            [ TengwaRune.LEFT_CURL, TengwaRune.ROMEN ], TengwaRune.ANNA,
+            TengwaRune.SILME, TengwaRune.TINCO, [ TengwaRune.LEFT_CURL, TengwaRune.ROMEN ], TengwaRune.ANNA,
             TengwaRune.SPACE,
             // of the
             TengwaRune.OF_THE, TengwaRune.SPACE,
             // destruction
-            TengwaRune.ANDO, [ TengwaRune.DOT, TengwaRune.SILME ],
-            TengwaRune.TINCO, TengwaRune.ROMEN,
+            TengwaRune.ANDO, [ TengwaRune.DOT, TengwaRune.SILME ], TengwaRune.TINCO, TengwaRune.ROMEN,
             [ TengwaRune.RIGHT_CURL, TengwaRune.QUESSE ], TengwaRune.TINCO,
-            [ TengwaRune.ACUTE, TengwaRune.SHORT_CARRIER ],
-            [ TengwaRune.LEFT_CURL, TengwaRune.NUMEN ], TengwaRune.SPACE,
+            [ TengwaRune.ACUTE, TengwaRune.SHORT_CARRIER ], [ TengwaRune.LEFT_CURL, TengwaRune.NUMEN ],
+            TengwaRune.SPACE,
             // of the
             TengwaRune.OF_THE, TengwaRune.NEWLINE,
 
             // Line 2
             // one
-            [ TengwaRune.LEFT_CURL, TengwaRune.NUMEN, TengwaRune.UNDER_DOT ],
-            TengwaRune.SPACE,
+            [ TengwaRune.LEFT_CURL, TengwaRune.NUMEN, TengwaRune.UNDER_DOT ], TengwaRune.SPACE,
             // ring
-            TengwaRune.ROMEN, [ TengwaRune.ACUTE, TengwaRune.NGWALME ],
-            TengwaRune.SPACE,
+            TengwaRune.ROMEN, [ TengwaRune.ACUTE, TengwaRune.NGWALME ], TengwaRune.SPACE,
             // and
             [ TengwaRune.BAR, TengwaRune.ANDO ], TengwaRune.SPACE,
             // the
             TengwaRune.THE, TengwaRune.SPACE,
             // downfall
-            TengwaRune.ANDO, [ TengwaRune.LEFT_CURL, TengwaRune.VALA ],
-            TengwaRune.NUMEN, TengwaRune.FORMEN,
-            [ TengwaRune.THREE_DOTS, TengwaRune.LAMBE, TengwaRune.UNDER_BAR ],
-            TengwaRune.SPACE,
+            TengwaRune.ANDO, [ TengwaRune.LEFT_CURL, TengwaRune.VALA ], TengwaRune.NUMEN, TengwaRune.FORMEN,
+            [ TengwaRune.THREE_DOTS, TengwaRune.LAMBE, TengwaRune.UNDER_BAR ], TengwaRune.SPACE,
             // of
             TengwaRune.OF, TengwaRune.SPACE,
             // sauron
-            TengwaRune.SILME,
-            [ TengwaRune.THREE_DOTS, TengwaRune.SHORT_CARRIER ],
-            [ TengwaRune.RIGHT_CURL, TengwaRune.ROMEN ],
-            [ TengwaRune.LEFT_CURL, TengwaRune.NUMEN ], TengwaRune.SPACE,
+            TengwaRune.SILME, [ TengwaRune.THREE_DOTS, TengwaRune.SHORT_CARRIER ],
+            [ TengwaRune.RIGHT_CURL, TengwaRune.ROMEN ], [ TengwaRune.LEFT_CURL, TengwaRune.NUMEN ], TengwaRune.SPACE,
             // at
             [ TengwaRune.THREE_DOTS, TengwaRune.TINCO ], TengwaRune.SPACE,
             // the
             TengwaRune.THE, TengwaRune.SPACE,
             // end
-            [ TengwaRune.DOT, TengwaRune.NUMEN ], TengwaRune.ANDO,
-            TengwaRune.SPACE,
+            [ TengwaRune.DOT, TengwaRune.NUMEN ], TengwaRune.ANDO, TengwaRune.SPACE,
             // of the
             TengwaRune.OF_THE, TengwaRune.SPACE,
             // third
-            TengwaRune.THULE, [ TengwaRune.ACUTE, TengwaRune.ORE ],
-            TengwaRune.ANDO, TengwaRune.SPACE,
+            TengwaRune.THULE, [ TengwaRune.ACUTE, TengwaRune.ORE ], TengwaRune.ANDO, TengwaRune.SPACE,
             // age.
-            [ TengwaRune.THREE_DOTS, TengwaRune.ANGA, TengwaRune.UNDER_DOT ],
-            TengwaRune.PERIOD, ],
+            [ TengwaRune.THREE_DOTS, TengwaRune.ANGA, TengwaRune.UNDER_DOT ], TengwaRune.PERIOD, ],
 
-    FONT_LETTERS: [ [ "5", "R" ], " ", "4", [ "8", "R" ], " ", "w",
-            [ "~", "Y" ], "z", " ", [ "8", "R" ], " ", "1", "7",
-            [ "i", "D", "&#201;" ], "2", " ", "e", [ "6", "R" ], "8", "1", " ",
-            "@", " ", "8", "1", [ "7", "Y" ], "h", " ", "W?", " ", "2",
-            [ "8", "T" ], "1", "7", [ "z", "&#225;" ], "1", [ "`", "R" ],
-            [ "5", "Y" ], " ", "W?", [ "5", "Y", "&#201;" ], " ", "7",
-            [ "b", "R" ], " ", [ "2", "{" ], " ", "@", " ", "2", [ "y", "Y" ],
-            "5", "e", [ "j", "D", "&#237;" ], " ", "W", " ", "8", [ "`", "D" ],
-            [ "7", "&#225;" ], [ "5", "Y" ], " ", [ "1", "D" ], " ", "@", " ",
-            [ "5", "T" ], "2", " ", "W?", " ", "3", [ "6", "R" ], "2", " ",
+    FONT_LETTERS: [ [ "5", "R" ], " ", "4", [ "8", "R" ], " ", "w", [ "~", "Y" ], "z", " ", [ "8", "R" ], " ", "1",
+            "7", [ "i", "D", "&#201;" ], "2", " ", "e", [ "6", "R" ], "8", "1", " ", "@", " ", "8", "1", [ "7", "Y" ],
+            "h", " ", "W?", " ", "2", [ "8", "T" ], "1", "7", [ "z", "&#225;" ], "1", [ "`", "R" ], [ "5", "Y" ], " ",
+            "W?", [ "5", "Y", "&#201;" ], " ", "7", [ "b", "R" ], " ", [ "2", "{" ], " ", "@", " ", "2", [ "y", "Y" ],
+            "5", "e", [ "j", "D", "&#237;" ], " ", "W", " ", "8", [ "`", "D" ], [ "7", "&#225;" ], [ "5", "Y" ], " ",
+            [ "1", "D" ], " ", "@", " ", [ "5", "T" ], "2", " ", "W?", " ", "3", [ "6", "R" ], "2", " ",
             [ "s", "D", "&#201;" ], "-" ],
 }
 
