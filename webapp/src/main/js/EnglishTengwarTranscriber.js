8dace16ed4ebf0d9839dfe5a5f89306c913385dc
Jeffrey M. Thompson <jmthompson@192.168.1.9>
2016-06-24 11:54:00 -0600

    Fixed multiple bugs so the example page works.

--- webapp/src/main/js/EnglishTengwarTranscriber.js
+++ webapp/src/main/js/EnglishTengwarTranscriber.js
@@ -32,6 +32,79 @@ define([ "EnglishToPhoneticForTengwarTranscriber", "PhoneticToEnglishForTengwarT
         }
     }
 
+    EnglishTengwarTranscriber.prototype.determineFontLetter = function(rune)
+    {
+        if (!rune) { throw new Error("rune " + rune + " is undefined"); }
+
+        var answer;
+
+        if (Array.isArray(rune))
+        {
+            answer = [];
+
+            for (var i = 0; i < rune.length; i++)
+            {
+                var rune2 = rune[i];
+                answer[answer.length] = this.determineFontLetter(rune2);
+            }
+        }
+        else
+        {
+            var properties = TengwaRune.properties;
+            try
+            {
+                var fontLetter = properties[rune].fontLetter;
+
+                if (fontLetter)
+                {
+                    answer = fontLetter;
+                }
+            }
+            catch (e)
+            {
+                LOGGER.error("Unknown rune " + rune);
+            }
+        }
+
+        return answer;
+    }
+
+    EnglishTengwarTranscriber.prototype.runesToFontLetters = function(runes)
+    {
+        var answer = [];
+
+        for (var i = 0; i < runes.length; i++)
+        {
+            var rune = runes[i];
+
+            if (!rune) { throw new Error("rune is undefined from " + i + " " + runes); }
+
+            if (rune !== "newline")
+            {
+                answer[answer.length] = this.determineFontLetter(rune);
+            }
+        }
+
+        return answer;
+    }
+
+    EnglishTengwarTranscriber.prototype.runesToLanguageWords = function(runes)
+    {
+        var phonemes = this.runesToPhonemes(runes);
+        var phonemesToLanguageTranscriber = new PhoneticToEnglishForTengwarTranscriber();
+        var languageWords = phonemesToLanguageTranscriber.phonemesToLanguageWords(phonemes);
+
+        return languageWords;
+    }
+
+    EnglishTengwarTranscriber.prototype.runesToPhonemes = function(runes)
+    {
+        var runesToPhonemesTranscriber = new TengwarToPhoneticTranscriber();
+        var phonemes = runesToPhonemesTranscriber.runesToPhonemes(runes);
+
+        return phonemes;
+    }
+
     /**
      * @param fromSequence
      *            From sequence.
--- webapp/src/main/js/example/TengwarExampleToHtml.js
+++ webapp/src/main/js/example/TengwarExampleToHtml.js
@@ -1,200 +1,204 @@
-var TengwarExampleToHtml = function(runesIn)
+define([ "EnglishTengwarTranscriber", "TengwaRune" ], function(EnglishTengwarTranscriber, TengwaRune)
 {
-    var runes = runesIn;
-    var transcriber = new EnglishTengwarTranscriber2();
-
-    this.getRunes = function()
+    var TengwarExampleToHtml = function(runesIn)
     {
-        return runes;
-    }
+        var runes = runesIn;
+        var transcriber = new EnglishTengwarTranscriber();
 
-    this.getTranscriber = function()
-    {
-        return transcriber;
-    }
-}
-
-TengwarExampleToHtml.prototype.generateFontRows = function()
-{
-    var letters = this.getTranscriber().runesToFontLetters(this.getRunes());
-
-    var answer = "<tr>\n";
-    answer += "<td class=\"table-header-cell-details\">Font</td>\n";
-
-    for (var i = 0; i < letters.length; i++)
-    {
-        var letter = letters[i];
-
-        if (letter !== "newline")
+        this.getRunes = function()
         {
-            answer += "<td class=\"table-cell-tengwar\">"
+            return runes;
+        }
 
-            if (Array.isArray(letter))
-            {
-                for (var j = 0; j < letter.length; j++)
-                {
-                    var letter2 = letter[j];
-                    answer += letter2;
-                }
-            }
-            else
-            {
-                answer += letter;
-            }
-
-            answer += "</td>\n"
+        this.getTranscriber = function()
+        {
+            return transcriber;
         }
     }
 
-    answer += "</tr>\n";
-
-    return answer;
-}
-
-TengwarExampleToHtml.prototype.generateRuneRows = function()
-{
-    var runes = this.getRunes();
-    var properties = TengwaRune.properties;
-
-    var answer = "<tr>\n";
-    answer += "<td class=\"table-header-cell-details\">Rune</td>\n";
-
-    for (var i = 0; i < runes.length; i++)
+    TengwarExampleToHtml.prototype.generateFontRows = function()
     {
-        var rune = runes[i];
+        var letters = this.getTranscriber().runesToFontLetters(this.getRunes());
 
-        if (rune !== "newline")
+        var answer = "<tr>\n";
+        answer += "<td class=\"table-header-cell-details\">Font</td>\n";
+
+        for (var i = 0; i < letters.length; i++)
         {
-            answer += "<td class=\"table-cell-rune-name\">";
+            var letter = letters[i];
 
-            if (rune === "space")
+            if (letter !== "newline")
             {
-                answer += " ";
-            }
-            else if (rune === "period")
-            {
-                answer += ".";
-            }
-            else if (Array.isArray(rune))
-            {
-                for (var j = 0; j < rune.length; j++)
+                answer += "<td class=\"table-cell-tengwar\">"
+
+                if (Array.isArray(letter))
                 {
-                    var rune2 = rune[j];
-                    answer += properties[rune2].name;
-
-                    if (j < rune.length - 1)
+                    for (var j = 0; j < letter.length; j++)
                     {
-                        answer += "-";
+                        var letter2 = letter[j];
+                        answer += letter2;
                     }
                 }
-            }
-            else
-            {
-                answer += properties[rune].name;
-            }
-
-            answer += "</td>\n";
-        }
-    }
-
-    answer += "</tr>\n";
-
-    return answer;
-}
-
-TengwarExampleToHtml.prototype.generatePhonemeRows = function()
-{
-    var letters = this.getTranscriber().runesToPhonemes(this.getRunes());
-
-    var answer = "<tr>\n";
-    answer += "<td class=\"table-header-cell-details\">Phoneme</td>\n";
-
-    for (var i = 0; i < letters.length; i++)
-    {
-        var letter = letters[i];
-
-        if (letter !== "newline")
-        {
-            answer += "<td class=\"table-cell-details\">";
-
-            if (Array.isArray(letter))
-            {
-                for (var j = 0; j < letter.length; j++)
+                else
                 {
-                    var letter2 = letter[j];
-                    answer += letter2;
+                    answer += letter;
                 }
-            }
-            else
-            {
-                answer += letter;
-            }
 
-            answer += "</td>\n";
+                answer += "</td>\n"
+            }
         }
+
+        answer += "</tr>\n";
+
+        return answer;
     }
 
-    answer += "</tr>\n";
-
-    return answer;
-}
-
-TengwarExampleToHtml.prototype.generateLanguageRows = function()
-{
-    var runes = this.getRunes();
-    var words = this.getTranscriber().runesToLanguageWords(runes);
-
-    var answer = "<tr>\n";
-    answer += "<td class=\"table-header-cell-details\">Language</td>\n";
-    var start = 0;
-
-    for (var i = 0; i < words.length; i++)
+    TengwarExampleToHtml.prototype.generateRuneRows = function()
     {
-        var word = words[i];
-        var colspan = 1;
+        var runes = this.getRunes();
+        var properties = TengwaRune.properties;
 
-        if (this.isPunctuation(word))
+        var answer = "<tr>\n";
+        answer += "<td class=\"table-header-cell-details\">Rune</td>\n";
+
+        for (var i = 0; i < runes.length; i++)
         {
-            for (var j = start; j < runes.length; j++)
+            var rune = runes[i];
+
+            if (rune !== "newline")
             {
-                if (this.isPunctuation(runes[j]))
+                answer += "<td class=\"table-cell-rune-name\">";
+
+                if (rune === "space")
                 {
-                    colspan = j - start;
-                    start = j + 1;
-                    break;
+                    answer += " ";
                 }
+                else if (rune === "period")
+                {
+                    answer += ".";
+                }
+                else if (Array.isArray(rune))
+                {
+                    for (var j = 0; j < rune.length; j++)
+                    {
+                        var rune2 = rune[j];
+                        answer += properties[rune2].name;
+
+                        if (j < rune.length - 1)
+                        {
+                            answer += "-";
+                        }
+                    }
+                }
+                else
+                {
+                    answer += properties[rune].name;
+                }
+
+                answer += "</td>\n";
             }
-
-            answer += "<td class=\"table-cell-details\" colspan=\"";
-            answer += colspan;
-            answer += "\">";
-            answer += words[i - 1];
-            answer += "</td>\n";
-
-            answer += "<td class=\"table-cell-details\">";
-            answer += word;
-            answer += "</td>\n";
         }
-        else if (i === words.length - 1)
-        {
-            colspan = runes.length - start;
 
-            answer += "<td class=\"table-cell-details\" colspan=\"";
-            answer += colspan;
-            answer += "\">";
-            answer += word;
-            answer += "</td>\n";
-        }
+        answer += "</tr>\n";
+
+        return answer;
     }
 
-    answer += "</tr>\n";
+    TengwarExampleToHtml.prototype.generatePhonemeRows = function()
+    {
+        var letters = this.getTranscriber().runesToPhonemes(this.getRunes());
 
-    return answer;
-}
+        var answer = "<tr>\n";
+        answer += "<td class=\"table-header-cell-details\">Phoneme</td>\n";
 
-TengwarExampleToHtml.prototype.isPunctuation = function(word)
-{
-    return word === TengwaRune.SPACE || word === TengwaRune.COMMA
-            || word === TengwaRune.PERIOD || word === TengwaRune.NEWLINE
-            || word === " " || word === "," || word === "." || word === "\n";
-}
+        for (var i = 0; i < letters.length; i++)
+        {
+            var letter = letters[i];
+
+            if (letter !== "newline")
+            {
+                answer += "<td class=\"table-cell-details\">";
+
+                if (Array.isArray(letter))
+                {
+                    for (var j = 0; j < letter.length; j++)
+                    {
+                        var letter2 = letter[j];
+                        answer += letter2;
+                    }
+                }
+                else
+                {
+                    answer += letter;
+                }
+
+                answer += "</td>\n";
+            }
+        }
+
+        answer += "</tr>\n";
+
+        return answer;
+    }
+
+    TengwarExampleToHtml.prototype.generateLanguageRows = function()
+    {
+        var runes = this.getRunes();
+        var words = this.getTranscriber().runesToLanguageWords(runes);
+
+        var answer = "<tr>\n";
+        answer += "<td class=\"table-header-cell-details\">Language</td>\n";
+        var start = 0;
+
+        for (var i = 0; i < words.length; i++)
+        {
+            var word = words[i];
+            var colspan = 1;
+
+            if (this.isPunctuation(word))
+            {
+                for (var j = start; j < runes.length; j++)
+                {
+                    if (this.isPunctuation(runes[j]))
+                    {
+                        colspan = j - start;
+                        start = j + 1;
+                        break;
+                    }
+                }
+
+                answer += "<td class=\"table-cell-details\" colspan=\"";
+                answer += colspan;
+                answer += "\">";
+                answer += words[i - 1];
+                answer += "</td>\n";
+
+                answer += "<td class=\"table-cell-details\">";
+                answer += word;
+                answer += "</td>\n";
+            }
+            else if (i === words.length - 1)
+            {
+                colspan = runes.length - start;
+
+                answer += "<td class=\"table-cell-details\" colspan=\"";
+                answer += colspan;
+                answer += "\">";
+                answer += word;
+                answer += "</td>\n";
+            }
+        }
+
+        answer += "</tr>\n";
+
+        return answer;
+    }
+
+    TengwarExampleToHtml.prototype.isPunctuation = function(word)
+    {
+        return word === TengwaRune.SPACE || word === TengwaRune.COMMA || word === TengwaRune.PERIOD ||
+                word === TengwaRune.NEWLINE || word === " " || word === "," || word === "." || word === "\n";
+    }
+
+    return TengwarExampleToHtml;
+});
