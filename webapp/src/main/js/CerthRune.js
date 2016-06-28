394422b36631145703d95ae0975403a06044c728
Jeffrey M. Thompson <jmthompson@192.168.1.9>
2016-06-28 11:56:12 -0600

    Added phonemes.

--- webapp/src/main/js/CerthRune.js
+++ webapp/src/main/js/CerthRune.js
@@ -78,384 +78,448 @@ define(function()
                 name: "C1",
                 displayName: "c1",
                 fontLetter: "p",
+                phoneme: "p",
             },
             "c2":
             {
                 name: "C2",
                 displayName: "c2",
                 fontLetter: "b",
+                phoneme: "b",
             },
             "c3":
             {
                 name: "C3",
                 displayName: "c3",
                 fontLetter: "f",
+                phoneme: "f",
             },
             "c4":
             {
                 name: "C4",
                 displayName: "c4",
                 fontLetter: "v",
+                phoneme: "v",
             },
             "c5":
             {
                 name: "C5",
                 displayName: "c5",
                 fontLetter: "W",
+                phoneme: "hw",
             },
             "c6":
             {
                 name: "C6",
                 displayName: "c6",
                 fontLetter: "m",
+                phoneme: "m",
             },
             "c7":
             {
                 name: "C7",
                 displayName: "c7",
                 fontLetter: "M",
+                phoneme: "mh",
             },
             "c8":
             {
                 name: "C8",
                 displayName: "c8",
                 fontLetter: "t",
+                phoneme: "t",
             },
             "c9":
             {
                 name: "C9",
                 displayName: "c9",
                 fontLetter: "d",
+                phoneme: "d",
             },
             "c10":
             {
                 name: "C10",
                 displayName: "c10",
                 fontLetter: "?",
+                phoneme: "th",
             },
             "c11":
             {
                 name: "C11",
                 displayName: "c11",
                 fontLetter: "?",
+                phoneme: "dh",
             },
             "c12":
             {
                 name: "C12",
                 displayName: "c12",
                 fontLetter: "r",
+                phoneme: "r",
             },
             "c13":
             {
                 name: "C13",
                 displayName: "c13",
                 fontLetter: "C",
+                phoneme: "ch",
             },
             "c14":
             {
                 name: "C14",
                 displayName: "c14",
                 fontLetter: "j",
+                phoneme: "j",
             },
             "c15":
             {
                 name: "C15",
                 displayName: "c15",
                 fontLetter: "S",
+                phoneme: "sh",
             },
             "c16":
             {
                 name: "C16",
                 displayName: "c16",
                 fontLetter: "Z",
+                phoneme: "zh",
             },
             "c17":
             {
                 name: "C17",
                 displayName: "c17",
                 fontLetter: "z",
+                phoneme: "z",
             },
             "c18":
             {
                 name: "C18",
                 displayName: "c18",
                 fontLetter: "k",
+                phoneme: "k",
             },
             "c19":
             {
                 name: "C19",
                 displayName: "c19",
                 fontLetter: "g",
+                phoneme: "g",
             },
             "c20":
             {
                 name: "C20",
                 displayName: "c20",
                 fontLetter: "K",
+                phoneme: "kh",
             },
             "c21":
             {
                 name: "C21",
                 displayName: "c21",
                 fontLetter: "G",
+                phoneme: "gh",
             },
             "c22":
             {
                 name: "C22",
                 displayName: "c22",
                 fontLetter: "n",
+                phoneme: "n",
             },
             "c23":
             {
                 name: "C23",
                 displayName: "c23",
                 fontLetter: "q",
+                phoneme: "kw",
             },
             "c24":
             {
                 name: "C24",
                 displayName: "c24",
                 fontLetter: "Q",
+                phoneme: "gw",
             },
             "c25":
             {
                 name: "C25",
                 displayName: "c25",
                 fontLetter: "=",
+                phoneme: "khw",
             },
             "c26":
             {
                 name: "C26",
                 displayName: "c26",
                 fontLetter: ">",
+                phoneme: "ghw",
             },
             "c27":
             {
                 name: "C27",
                 displayName: "c27",
                 fontLetter: "?",
+                phoneme: "ngw",
             },
             "c28":
             {
                 name: "C28",
                 displayName: "c28",
                 fontLetter: "/",
+                phoneme: "nw",
             },
             "c29":
             {
                 name: "C29",
                 displayName: "c29",
                 fontLetter: "J",
+                phoneme: "j",
             },
             "c30":
             {
                 name: "C30",
                 displayName: "c30",
                 fontLetter: "R",
+                phoneme: "zh",
             },
             "c31":
             {
                 name: "C31",
                 displayName: "c31",
                 fontLetter: "l",
+                phoneme: "l",
             },
             "c32":
             {
                 name: "C32",
                 displayName: "c32",
                 fontLetter: "L",
+                phoneme: "lh",
             },
             "c33":
             {
                 name: "C33",
                 displayName: "c33",
                 fontLetter: "D",
+                phoneme: "nd",
             },
             "c34":
             {
                 name: "C34",
                 displayName: "c34",
                 fontLetter: "h",
+                phoneme: "s",
             },
             "c35":
             {
                 name: "C35",
                 displayName: "c35",
                 fontLetter: "s",
+                phoneme: "s",
             },
             "c36":
             {
                 name: "C36",
                 displayName: "c36",
                 fontLetter: "N",
+                phoneme: "ng",
             },
             "c37":
             {
                 name: "C37",
                 displayName: "c37",
                 fontLetter: "[",
+                phoneme: "ng",
             },
             "c38":
             {
                 name: "C38",
                 displayName: "c38",
                 fontLetter: "\\",
+                phoneme: "nd",
             },
             "c39":
             {
                 name: "C39",
                 displayName: "c39",
                 fontLetter: "i",
+                phoneme: "i",
             },
             "c40":
             {
                 name: "C40",
                 displayName: "c40",
                 fontLetter: "y",
+                phoneme: "y",
             },
             "c41":
             {
                 name: "C41",
                 displayName: "c41",
                 fontLetter: "Y",
+                phoneme: "hy",
             },
             "c42":
             {
                 name: "C42",
                 displayName: "c42",
                 fontLetter: "u",
+                phoneme: "u",
             },
             "c43":
             {
                 name: "C43",
                 displayName: "c43",
                 fontLetter: "U",
+                phoneme: "u",
             },
             "c44":
             {
                 name: "C44",
                 displayName: "c44",
                 fontLetter: "w",
+                phoneme: "w",
             },
             "c45":
             {
                 name: "C45",
                 displayName: "c45",
                 fontLetter: "ü",
+                phoneme: "u",
             },
             "c46":
             {
                 name: "C46",
                 displayName: "c46",
                 fontLetter: "e",
+                phoneme: "e",
             },
             "c47":
             {
                 name: "C47",
                 displayName: "c47",
                 fontLetter: "E",
+                phoneme: "e",
             },
             "c48":
             {
                 name: "C48",
                 displayName: "c48",
                 fontLetter: "a",
+                phoneme: "a",
             },
             "c49":
             {
                 name: "C49",
                 displayName: "c49",
                 fontLetter: "A",
+                phoneme: "a",
             },
             "c50":
             {
                 name: "C50",
                 displayName: "c50",
                 fontLetter: "o",
+                phoneme: "o",
             },
             "c51":
             {
                 name: "C51",
                 displayName: "c51",
                 fontLetter: "O",
+                phoneme: "oo",
             },
             "c52":
             {
                 name: "C52",
                 displayName: "c52",
                 fontLetter: "ö",
+                phoneme: "o",
             },
             "c53":
             {
                 name: "C53",
                 displayName: "c53",
                 fontLetter: "~",
+                phoneme: "n",
             },
             "c54":
             {
                 name: "C54",
                 displayName: "c54",
                 fontLetter: "{",
+                phoneme: "s",
             },
             "c55":
             {
                 name: "C55",
                 displayName: "c55",
                 fontLetter: "ë",
+                phoneme: "e",
             },
             "c56":
             {
                 name: "C56",
                 displayName: "c56",
                 fontLetter: "}",
+                phoneme: "o",
             },
             "c57":
             {
                 name: "C57",
                 displayName: "c57",
                 fontLetter: "P",
+                phoneme: "ps",
             },
             "c58":
             {
                 name: "C58",
                 displayName: "c58",
                 fontLetter: "T",
+                phoneme: "ts",
             },
             "c59":
             {
                 name: "C59",
                 displayName: "c59",
                 fontLetter: "|",
+                phoneme: "h",
             },
             "c60":
             {
                 name: "C60",
                 displayName: "c60",
                 fontLetter: "&",
+                phoneme: "&",
             },
             "space":
             {
                 name: "SPACE",
                 displayName: "space",
                 fontLetter: ".",
+                phoneme: " ",
             },
             "comma":
             {
                 name: "COMMA",
                 displayName: "comma",
                 fontLetter: ",",
+                phoneme: ",",
             },
             "period":
             {
                 name: "PERIOD",
                 displayName: "period",
                 fontLetter: ":",
+                phoneme: ".",
             },
             "newline":
             {
                 name: "NEWLINE",
                 displayName: "newline",
                 fontLetter: "\n",
+                phoneme: "\n",
             },
         },
 
